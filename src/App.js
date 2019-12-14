149b98cd225ab5fef1be0e35c19acfaeb92a0721
HGChamara <hgchamara1991@gmail.com>
2019-12-15 04:55:44 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DashBoard from './components/DashBoard';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
+import Header from './Layout/Header';
+import {Provider} from "react-redux";
+import store from './store';
 
 function App() {
   return (
+    <Provider store={store}>
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/>
+      <Route exact path="/dashboard" component={DashBoard}/>
+      <Route exact path="/addProject" component={AddProject}/>
     </div>
+    </Router>
+    </Provider>
   );
 }
 
 export default App;
+
